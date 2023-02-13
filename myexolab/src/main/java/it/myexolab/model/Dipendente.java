60eecad3bf6f3d2e1e48d9b2e2f8807170b085cc
f-federico <f.furfaro3@gmail.com>
2023-02-13 17:39:04 +0100

    prima commit

--- myexolab/src/main/java/it/myexolab/model/Dipendente.java
+++ myexolab/src/main/java/it/myexolab/model/Dipendente.java
@@ -2,62 +2,51 @@ package it.myexolab.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Data;
 
-//@Data
-
-@Document
-//@SuppressWarnings("unused")
+//@Document(collection = "dipendente")
 public class Dipendente {
-     @Id
+	@Id
 	private String id;
+//	@Field(name = "nome")
 	private String nome;
+//	@Field(name = "cognome")
 	private String cognome;
+//	@Field(name = "eta")
 	private int eta;
-	private String ruolo;
-	
-	private String lemiemodifiche;
-	
-	private String aggroba;
-	
-	private String vedisefunje;
-	
-	
-	
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCognome() {
 		return cognome;
 	}
+
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+
 	public int getEta() {
 		return eta;
 	}
+
 	public void setEta(int eta) {
 		this.eta = eta;
 	}
-	public String getRuolo() {
-		return ruolo;
-	}
-	public void setRuolo(String ruolo) {
-		this.ruolo = ruolo;
-	}
-	
-	
-	
-	
-	
+
 }
