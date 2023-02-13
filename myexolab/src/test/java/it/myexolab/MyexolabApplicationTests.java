432ec8d5373d76bab8d2c8ef88c150a5fd4c8dcf
f-federico <125191760+f-federico@users.noreply.github.com>
2023-02-13 15:39:13 +0100

    Add files via upload

---
+++ myexolab/src/test/java/it/myexolab/MyexolabApplicationTests.java
@@ -0,0 +1,13 @@
+package it.myexolab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyexolabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
