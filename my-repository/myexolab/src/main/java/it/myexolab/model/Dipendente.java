6e4a053cccf1b579563695951fc1dd45c880d7e9
f-federico <f.furfaro3@gmail.com>
2023-02-13 15:24:38 +0100

    ok

---
+++ my-repository/myexolab/src/main/java/it/myexolab/model/Dipendente.java
@@ -0,0 +1,63 @@
+package it.myexolab.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+//@Data
+
+@Document
+//@SuppressWarnings("unused")
+public class Dipendente {
+     @Id
+	private String id;
+	private String nome;
+	private String cognome;
+	private int eta;
+	private String ruolo;
+	
+	private String lemiemodifiche;
+	
+	private String aggroba;
+	
+	private String vedisefunje;
+	
+	private String buiiiiiiiiiii;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public int getEta() {
+		return eta;
+	}
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+	public String getRuolo() {
+		return ruolo;
+	}
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+	
+	
+	
+	
+	
+}
---
+++ my-repository/myexolab/src/main/java/it/myexolab/repository/DipendenteRepository.java
@@ -0,0 +1,31 @@
+package it.myexolab.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import it.myexolab.model.Dipendente;
+
+@Repository
+public interface DipendenteRepository extends MongoRepository<Dipendente,String>{
+
+	@Query("{'nome':?0}")
+	List<Dipendente> findByNome(String nome);
+	
+	@Query("{'ruolo':?0}")
+	List<Dipendente> findByRuolo(String ruolo);
+//	case insensitive
+	@Query("{'$regex':?0,'$options':'i'}")
+	List<Dipendente> findByCognome(String cognome);
+	
+	@Query(value="{'eta':?0},{'cognome':?1}", fields="{'id':0}")
+	List<Dipendente> findByEtaAndCognome(int eta, String cognome);
+	
+	@Query("{'eta':{'$gte':?0,'$lte':?1}}")
+	List<Dipendente> findDipendenteByEta(int min, int max);
+	
+//	List<Dipendente> findByRuoloOrderByNome();
+	
+}
